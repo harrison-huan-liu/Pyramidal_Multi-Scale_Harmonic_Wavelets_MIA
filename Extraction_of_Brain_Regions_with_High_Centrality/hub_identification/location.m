1e0678cd246c19a7911e7ebc2fced7c544d5278e
Huan Liu <38698212+harrison-huan-liu@users.noreply.github.com>
2024-02-18 18:44:02 +0800

    Add files via upload

---
+++ Extraction_of_Brain_Regions_with_High_Centrality/hub_identification/location.m
@@ -0,0 +1,243 @@
+% close all;clear;clc
+%% load brain connectivity network
+p=55;% p is the number of eignvectors %55
+Graph=Preprocess_network_data('Data\DataTS.csv','Data\AD-Data\',p);
+
+%% txt
+destriux_148 = readcell('destriux_148.xlsx');
+hub_nodes = [3, 6, 10, 15, 16, 23, 24, 27, 30, 35, 44, 48, 51, 55, 56, 59, 65, 66, 68, 69, 73, 77, 80, 84, 89, 90, 93, 97, 98, 104, 118, 122, 123, 127, 128, 129, 140, 142, 144, 147, 155];
+% x = cell(148,1);
+% y = cell(148,1);
+% z = cell(148,1);
+% color = cell(148,1);
+% degree = cell(148,1);
+node = destriux_148;
+hub_nodes = [Sum_norm_sort_left(1:40),Sum_norm_sort_right(1:40)];
+
+j = 1;
+% temp = degrees_und(Graph(1).W);
+temp = Sum_norm;
+for i = 1:148
+    if i == hub_nodes(j)
+        node{j,1} = destriux_148{hub_nodes(j),1}/1.1;
+        node{j,2} = destriux_148{hub_nodes(j),2}/1.1;
+        node{j,3} = destriux_148{hub_nodes(j),3}/1.1;
+%         color{i} = 2;
+%         node{i,4} = color{i};
+%         temp = degrees_und(Graph(1).W);
+%         degree{i} = temp(hub_nodes(j));
+%         node{i,5} = degree{i};
+        node{j,4} = 1.5 + 0.5*rand;
+        node{j,5} = temp(hub_nodes(j));
+        node{j,6} = destriux_148{hub_nodes(j),6};
+        j = j + 1;
+    else
+%         node{i,1} = destriux_148{i,1};
+%         node{i,2} = destriux_148{i,2};
+%         node{i,3} = destriux_148{i,3};
+% %         color{i} = 1;
+% %         node{i,4} = color{i};
+% %         temp = degrees_und(Graph(1).W);
+% %         degree{i} = temp(hub_nodes(i));
+% %         node{i,5} = degree{i};
+%         node{i,5} = temp(i);
+    end
+end
+
+writecell(node,'hub40_randi_520.txt','Delimiter','tab')
+% load MST
+% writematrix(MST{139},'edge40.txt','Delimiter','tab')
+
+%% degree 148 nodes
+p=55;% p is the number of eignvectors %55
+Graph=Preprocess_network_data('Data\DataTS.csv','Data\AD-Data\',p);
+CommonNetwork = zeros(n);
+for i = 1:138
+    CommonNetwork=CommonNetwork+Graph(i).W;
+end
+CommonNetwork=CommonNetwork/m;
+CommonNetwork(CommonNetwork<0.002)=0;
+CommonNetwork=(CommonNetwork+CommonNetwork')/2;
+temp = degrees_und(CommonNetwork);
+destriux_148 = readcell('destriux_148.xlsx');
+node = destriux_148;
+for i = 1:148
+    node{i,1} = destriux_148{i,1}/1.1;
+    node{i,2} = destriux_148{i,2}/1.1;
+    node{i,3} = destriux_148{i,3}/1.1;
+    node{i,4} = temp(i)/148;
+    node{i,5} = temp(i)/148;
+    node{i,6} = destriux_148{i,6};
+end
+
+writecell(node,'node148_degree.txt','Delimiter','tab')
+
+load b725
+load b727
+node_a = cell(9,1);
+node_n = zeros(1,9);
+node_index = cell(9,1);
+for i = 1:9
+    node_index{i,1} = find(abs(b725(:,i))>0);
+    node_index{i,1} = union(node_index{i,1},b727(i));
+    node_a{i,1} = destriux_148(node_index{i,1},:);
+    node_n(1,i) = length(node_index{i,1});
+    for j = 1:node_n(1,i)
+        node_a{i,1}{j,6} = b725(node_index{i,1}(j),i);
+        if b727(1,i) == (node_index{i,1}(j))
+            node_a{i,1}{j,4} = 2;
+            node_a{i,1}{j,5} = 3;
+        end
+    end
+end
+for i = 1:9
+    filename = ['node_',num2str(i),'.txt'];
+    writecell(node_a{i,1},filename,'Delimiter','tab');
+end
+
+
+CommonNetwork = zeros(n);
+for i = 1:m
+    CommonNetwork=CommonNetwork+Graph(i).W;
+end
+CommonNetwork=CommonNetwork/m;
+CommonNetwork(CommonNetwork<0.002)=0;
+CommonNetwork=(CommonNetwork+CommonNetwork')/2;
+edge_a = cell(9,1);
+for i = 1:9
+    edge_a{i,1} = zeros(node_n(1,i),node_n(1,i));
+    for j = 1:node_n(1,i)
+        for k = 1:node_n(1,i)
+            edge_a{i,1}(j,k) = CommonNetwork(node_index{i,1}(j),node_index{i,1}(k));
+        end
+    end
+end
+for i = 1:9
+    filename = ['edge_',num2str(i),'.txt'];
+    writematrix(edge_a{i,1},filename,'Delimiter','tab');
+end
+
+sign_data = cell(3,1);
+for i = 1:3
+    sign_data{i,1} = destriux_148;
+    for j = 1:148
+        sign_data{i,1}{j,4}=AD_amy.Amyloid(j,i);
+        sign_data{i,1}{j,5}=AD_amy.Amyloid(j,i);
+    end
+end
+for i = 1:3
+    filename = ['sign_data_',num2str(i),'.txt'];
+    writecell(sign_data{i,1},filename,'Delimiter','tab');
+end
+
+
+%% framework picture
+b728 = zeros(148,9);
+for i = 1:9
+    b728(:,i) = multiPhi(:,6*(i-1)+1);
+end
+b729 = [3,3,3,6,6,6,10,10,10];
+
+% for i = 1:148
+%     for j = 1:9
+%         if abs(b728(i,j))<0.1
+%             b728(i,j) = 0;
+%         end
+%     end
+% end
+
+for i = 1:2
+    for j = 1
+        if abs(b728(i,j))<0.1
+            b728(i,j) = 0;
+        end
+    end
+end
+
+for i = 4:148
+    for j = 1
+        if abs(b728(i,j))<0.1
+            b728(i,j) = 0;
+        end
+    end
+end
+
+for i = 1:5
+    for j = 2
+        if abs(b728(i,j))<0.1
+            b728(i,j) = 0;
+        end
+    end
+end
+
+for i = 7:148
+    for j = 2
+        if abs(b728(i,j))<0.1
+            b728(i,j) = 0;
+        end
+    end
+end
+
+for i = 1:9
+    for j = 3
+        if abs(b728(i,j))<0.1
+            b728(i,j) = 0;
+        end
+    end
+end
+
+for i = 11:148
+    for j = 3
+        if abs(b728(i,j))<0.1
+            b728(i,j) = 0;
+        end
+    end
+end
+
+%% out
+
+fnode_a = cell(9,1);
+fnode_n = zeros(1,9);
+fnode_index = cell(9,1);
+for i = 1:9
+    fnode_n(1,i) = length(find(abs(b728(:,i))>0));
+end
+for i = 1:9
+    fnode_index{i,1} = find(abs(b728(:,i))>0);
+    fnode_a{i,1} = destriux_148(fnode_index{i,1},:);
+    for j = 1:fnode_n(1,i)
+        fnode_a{i,1}{j,6} = b728(fnode_index{i,1}(j),i);
+        if b729(1,i) == (fnode_index{i,1}(j))
+            fnode_a{i,1}{j,4} = 2;
+        end
+    end
+end
+for i = 1:9
+    filename = ['fnode_',num2str(i),'.txt'];
+    writecell(fnode_a{i,1},filename,'Delimiter','tab');
+end
+
+
+CommonNetwork = zeros(n);
+for i = 1:m
+    CommonNetwork=CommonNetwork+Graph(i).W;
+end
+CommonNetwork=CommonNetwork/m;
+CommonNetwork(CommonNetwork<0.002)=0;
+CommonNetwork=(CommonNetwork+CommonNetwork')/2;
+fedge_a = cell(9,1);
+for i = 1:9
+    fedge_a{i,1} = zeros(fnode_n(1,i),fnode_n(1,i));
+    for j = 1:fnode_n(1,i)
+        for k = 1:fnode_n(1,i)
+            fedge_a{i,1}(j,k) = CommonNetwork(fnode_index{i,1}(j),fnode_index{i,1}(k));
+        end
+    end
+end
+for i = 1:9
+    filename = ['fedge_',num2str(i),'.txt'];
+    writematrix(fedge_a{i,1},filename,'Delimiter','tab');
+end
+
+save('b728.mat','b728');
+save('b729.mat','b729');
---
+++ Extraction_of_Brain_Regions_with_High_Centrality/hub_identification/main.m
@@ -0,0 +1,193 @@
+% close all;clear;clc
+%% load brain connectivity network
+p=55;% p is the number of eignvectors %55
+Graph=Preprocess_network_data('Data\DataTS.csv','Data\AD-Data\',p);
+m = size(Graph,2);%m是研究对象的数目
+n = size(Graph(1).L,1);%每个研究对象的结点数目
+MST = cell(m+2,1);
+
+%% MST
+for i = 1:m+2
+    MST{i} = zeros(148,148);
+end
+
+for i = 1:m
+    M_connectivity = tril(Graph(i).W);
+    j = 1;
+    while j <= n-1
+        [maximum_column,index_column] = max(M_connectivity,[],1);
+        [maximun,index] = max(maximum_column);
+%         temp = M_connectivity(index_column(index),index);
+        M_connectivity(index_column(index),index) = 0;
+        MST{i}(index_column(index),index) = maximun;
+        MST{i}(index,index_column(index)) = maximun;
+        t = MST{i};
+        for k = 2:size(MST{i}, 1)
+            t = t | (t*MST{i});
+        end
+ 
+        if any(diag(t))
+            acc = 1;
+        else
+            acc = 0;
+        end
+        
+        if acc == 0 %the reason of 0
+            MST{i}(index_column(index),index) = 0;
+            MST{i}(index,index_column(index)) = 0;
+            j = j-1;
+        end
+        j = j+1;
+    end
+%     MST{i} = MST{i} + MST{i}';
+end
+
+for i = 1:m
+    MST{m+1} = MST{m+1}() + MST{i}();
+end
+
+
+
+% CommonNetwork = zeros(n);
+Betweenness_hub = zeros(m,n);
+Pagerank_hub = zeros(m,n);
+Participation_hub = zeros(m,n);
+Degree_hub = zeros(m,n);
+% for i = 1:m
+%     CommonNetwork=CommonNetwork+Graph(i).W;
+% end
+
+% for j = 1:m
+%     Betweenness_hub(j,:) = betweenness_wei(Graph(j).W)';
+%     Pagerank_hub(j,:) = pagerank_centrality(Graph(j).W,0.85,Graph(j).W)';
+%     [community_vector, community_statistic] = community_louvain(Graph(j).W);
+%     Participation_hub(j,:) = participation_coef(Graph(j).W,community_vector,0)';
+%     Degree_hub(j,:) = degrees_und(Graph(j).W);
+% end
+
+% CommonNetwork=CommonNetwork/m;
+% CommonNetwork(CommonNetwork<0.02)=0;
+% CommonNetwork=(CommonNetwork+CommonNetwork')/2;
+Betweenness_node = betweenness_wei(MST{m+1});
+Pagerank_node = pagerank_centrality(MST{m+1},0.85,MST{m+1})';
+[community_vector, community_statistic] = community_louvain(MST{m+1});
+Participation_node = participation_coef(MST{m+1},community_vector,0)';
+Degree_node = degrees_und(MST{m+1});
+Efficiency_node = efficiency_wei(MST{m+1},2);
+Clustering_coef_node = clustering_coef_wd(MST{m+1});
+
+Betweenness_node_norm = normalize(Betweenness_node,'range');
+Pagerank_node_norm = normalize(Pagerank_node,'range');
+Participation_node_norm = normalize(Participation_node,'range');
+Degree_node_norm = normalize(Degree_node,'range');
+Sum_norm = Betweenness_node_norm' + Pagerank_node_norm + Participation_node_norm + Degree_node_norm;
+Sum_norm_left = Sum_norm(1:74);
+Sum_norm_right = Sum_norm(75:148);
+[Sum_norm_rank,Sum_norm_sort] = sort(Sum_norm,'descend');
+[~,Sum_norm_sort_left] = sort(Sum_norm_left,'descend');
+[~,Sum_norm_sort_right] = sort(Sum_norm_right,'descend');
+
+
+Betweenness_node_norm = Betweenness_node_norm(Sum_norm_sort);
+Pagerank_node_norm = Pagerank_node_norm(Sum_norm_sort);
+Participation_node_norm = Participation_node_norm(Sum_norm_sort);
+Degree_node_norm = Degree_node_norm(Sum_norm_sort);
+
+writematrix(Betweenness_node_norm','Betweenness_node_norm.xls')
+writematrix(Pagerank_node_norm,'Pagerank_node_norm.xls')
+writematrix(Participation_node_norm,'Participation_node_norm.xls')
+writematrix(Degree_node_norm,'Degree_node_norm.xls')
+writematrix(Sum_norm_sort,'Sum_norm_sort.xls')
+
+Betweenness_hub_mean = mean(Betweenness_hub);
+Pagerank_hub_mean = mean(Pagerank_hub);
+Participation_hub_mean = mean(Participation_hub);
+Degree_hub_mean = mean(Degree_hub);
+
+[Betweenness_hub_rank,Betweenness_hub_sort] = sort(Betweenness_hub_mean,'descend');
+[Pagerank_hub_rank,Pagerank_hub_sort] = sort(Pagerank_hub_mean,'descend');
+[Participation_hub_rank,Participation_hub_sort] = sort(Participation_hub_mean,'descend');
+[Degree_hub_rank,Degree_hub_sort] = sort(Degree_hub_mean,'descend');
+
+% for k = 1:n
+% end
+
+Betweenness_hub = Betweenness_hub(:,Betweenness_hub_sort);
+Pagerank_hub = Pagerank_hub(:,Pagerank_hub_sort);
+Participation_hub = Participation_hub(:,Participation_hub_sort);
+Degree_hub = Degree_hub(:,Degree_hub_sort);
+
+writematrix(Betweenness_hub,'Betweenness_hub.xls')
+writematrix(Pagerank_hub,'Pagerank_hub.xls')
+writematrix(Participation_hub,'Participation_hub.xls')
+writematrix(Degree_hub,'Degree_hub.xls')
+
+hub_nodes = [3, 6, 10, 15, 16, 23, 24, 27, 30, 35, 44, 48, 51, 55, 56, 59, 65, 66, 68, 69, 73, 77, 80, 84, 89, 90, 93, 97, 98, 118, 122, 123, 127, 128, 129, 104, 140, 142, 144, 147];
+Betweenness_hub_is = ismember(Betweenness_hub_sort,hub_nodes);
+Pagerank_hub_is = ismember(Pagerank_hub_sort,hub_nodes);
+Participation_hub_is = ismember(Participation_hub_sort,hub_nodes);
+Degree_hub_is = ismember(Degree_hub_sort,hub_nodes);
+
+Betweenness_hub_int = zeros(1,148);
+Pagerank_hub_int = zeros(1,148);
+Participation_hub_int = zeros(1,148);
+Degree_hub_int = zeros(1,148);
+
+Betweenness_hub_int(Betweenness_hub_is) = 1;
+Pagerank_hub_int(Pagerank_hub_is) = 1;
+Participation_hub_int(Participation_hub_is) = 1;
+Degree_hub_int(Degree_hub_is) = 1;
+
+writematrix(Betweenness_hub_int,'Betweenness_hub_int.xls')
+writematrix(Pagerank_hub_int,'Pagerank_hub_int.xls')
+writematrix(Participation_hub_int,'Participation_hub_int.xls')
+writematrix(Degree_hub_int,'Degree_hub_int.xls')
+
+%% betweenness
+% Betweenness_node = zeros(m,n);
+% for i=1:m
+%     Betweenness_node(i,:) = betweenness_wei(Graph(i).W);
+% end
+% 
+% %% pagerank
+% Pagerank_node = zeros(m,n);
+% for i=1:m
+%     Pagerank_node(i,:) = pagerank_centrality(Graph(i).W,0.85,Graph(i).W);
+% end
+% 
+% %% participation coefficient
+% Participation_coefficient_node = zeros(m,n);
+% for i = 1:m
+%     [community_vector, community_statistic] = community_louvain(Graph(i).W);
+%     Participation_coefficient_node(i,:) = participation_coef(Graph(i).W,community_vector,0);
+% end
+% 
+% %% degree
+% Degree_node = zeros(m,n);
+% for i = 1:m
+%     Graph(i).W(Graph(i).W~=0) = 1;
+%     Degree_node(i,:) = degrees_und(Graph(i).W);
+% end
+
+%% select hub nodes
+% Betweenness = mean(Betweenness_node);
+% Pagerank = mean(Pagerank_node);
+% Participation_coefficient = mean(Participation_coefficient_node);
+% Degree = mean(Degree_node);
+
+Betweenness_mean = mean(Betweenness_node);
+Pagerank_mean = mean(Pagerank_node);
+Participation_coefficient_mean = mean(Participation_coefficient_node);
+Degree_mean = mean(Degree_node);
+
+Betweenness_sort = sort(Betweenness_node,'descend');
+Pagerank_sort = sort(Pagerank_node,'descend');
+Participation_coefficient_sort = sort(Participation_coefficient_node,'descend');
+Degree_sort = sort(Degree_node,'descend');
+
+Betweenness_index = find(Betweenness_node>=Betweenness_sort(12));% Betweenness_mean+450
+Pagerank_index = find(Pagerank_node>=Pagerank_sort(12));% Pagerank_mean+0.0025
+Participation_coefficient_index = find(Participation_coefficient_node>=Participation_coefficient_sort(12));% Participation_coefficient_mean+0.23
+Degree_index = find(Degree_node>=Degree_sort(12));% Degree_mean+6.2
+
+hub_nodes = [3, 6, 10, 15, 16, 23, 24, 27, 30, 35, 44, 48, 51, 55, 56, 59, 65, 66, 68, 69, 73, 77, 80, 84, 89, 90, 93, 97, 98, 118, 122, 123, 127, 128, 129, 104, 140, 142, 144, 147];
